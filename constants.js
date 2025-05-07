a62febf5739cd10178890334256d0bc878ab8b2d
Ishwari702 <ishwarigawande702@gmail.com>
2025-05-07 21:50:56 +0530

    Add files via upload

---
+++ constants.js
@@ -0,0 +1,19 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+import { getDatabase, ref, set, get, child } from "https://www.gstatic.com/firebasejs/11.6.1/firebase-app.js";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8UJu8JVuGTySyJhGN8YbmQuxXiCbKEqA",
+    authDomain: "login-with-firebase-74542.firebaseapp.com",
+    databaseURL: "https://login-with-firebase-74542-default-rtdb.firebaseio.com",
+    projectId: "login-with-firebase-74542",
+    storageBucket: "login-with-firebase-74542.appspot.com",
+    messagingSenderId: "537948051051",
+    appId: "1:537948051051:web:e93e9f54e4e2c2316ec32e",
+    measurementId: "G-ZVDZ9GJTYP"
+  };
+  // initialize firebase
+  firebase.initializeApp(firebaseConfig);
+  const services = firebase.database().ref("services");
+  const users = firebase.database().ref("users");
+  
